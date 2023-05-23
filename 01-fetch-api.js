7d75a4fc0100ffd61fd42a367b5eb65a43f7fa90
skifozzze <skifozzze@gmail.com>
2023-05-23 15:04:56 +0300

    upd

--- 01-fetch-api.js
+++ 01-fetch-api.js
@@ -59,13 +59,13 @@ function onFetchError(error) {
 
 // =========================================
 
-// const url = 'https://newsapi.org/v2/everything?q=cars';
-// const options = {
-//   headers: {
-//     Authorization: '4330ebfabc654a6992c2aa792f3173a3',
-//   },
-// };
+const url = 'https://newsapi.org/v2/everything?q=war';
+const options = {
+  headers: {
+    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+  },
+};
 
-// fetch(url, options)
-//   .then(r => r.json())
-//   .then(console.log);
+fetch(url, options)
+  .then(r => r.json())
+  .then(console.log);
